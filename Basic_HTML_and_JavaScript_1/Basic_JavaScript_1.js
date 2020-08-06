959a5de55edf6cebacee3c5ee099f8f5bbc1eca1
catdur68 <66445106+catdur68@users.noreply.github.com>
2020-08-06 12:37:58 -0700

    Repost to GitHub

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+/*Reposting to GitHub*/
+alert("Hello World! I am an alert box");
+
+function My_first_function()
+	{
+	var String = "Kiss me, I'm Irish";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+	}
---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -0,0 +1,9 @@
+/*Reposting to GitHub*/
+
+	function My_first_function()
+	{
+	var String = "Kiss me, I'm Irish";
+	var result = String.fontcolor("green");
+	document.getElementById("Irish").innerHTML = result;
+	}
+
